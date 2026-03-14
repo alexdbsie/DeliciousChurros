ed76ea8bb8d0150cdf3872002803a01b3d568898
alexdbsie <20093197@mydbs.ie>
2026-03-14 13:18:27 +0000

    Add Churros class with properties and constructor

---
+++ DeliciousChurros/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Churros
+{
+    public string Flavour { get; private set; }
+    public double Price { get; private set; }
+
+    public Churros(string Flavour, double price)
+    {
+        Flavour = flavour;
+        Price = price;
+    }
+}
